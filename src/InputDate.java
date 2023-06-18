c4ce1f146d5b07e9c9d228d6f08ddf11a3eda807
Your Name <you@example.com>
2023-06-18 18:00:56 +0400

    logifinal

--- src/InputDate.java
+++ src/InputDate.java
@@ -16,19 +16,19 @@ public class InputDate {
         }
         String[] birthNumbers = parseString[3].split("\\.");
         if (birthNumbers.length != 3) {
-            throw new Exception("Нарушен формат ввода даты рождения. Код ошибки 203");
+            throw new Exception("Неправильный формат ввода даты рождения. Код ошибки 203");
         }
         if (birthNumbers[0].length() != 2 || birthNumbers[1].length() != 2) {
-            throw new Exception("Нарушен формат ввода даты рождения. Код ошибки 204");
+            throw new Exception("Неправильный формат ввода даты рождения. Код ошибки 204");
         }
         if (birthNumbers[2].length() != 4) {
-            throw new Exception("Нарушен формат ввода даты рождения. Код ошибки 205");
+            throw new Exception("Неправильный формат ввода даты рождения. Код ошибки 205");
         }
         if (!parseString[4].matches("\\d+")) {
-            throw new Exception("Ошибка при вводе номера телефона. Код ошибки 206");
+            throw new Exception("Неправильный формат ввода номера телефона. Код ошибки 206");
         }
         if (!parseString[5].matches("[FfMm]")) {
-            throw new Exception("Ошибка выбора пола. Код ошибки 207");
+            throw new Exception("Неправильный формат ввода пола. Код ошибки 207");
         }
         return parseString;
     }
